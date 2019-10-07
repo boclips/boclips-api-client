163ab3f9ff82fcd3dad1326a199d238669bc83b9
Eva Varga <eva@boclips.com>
2019-10-07 16:20:36 +0100

    Export both Test and Http implementations

--- src/BoclipsApiClient.ts
+++ src/BoclipsApiClient.ts
@@ -1,25 +1,5 @@
-import { AxiosInstance } from 'axios';
-import { HttpBoclipsApiClient } from './HttpBoclipsApiClient';
 import { LegalRestrictions } from './LegalRestrictions';
-import { TestBoclipsApiClient } from './TestBoclipsApiClient';
 
 export interface BoclipsApiClient {
   getAllLegalRestrictions(): Promise<LegalRestrictions[]>;
 }
-
-let instance = null;
-
-export default {
-  getInstance: (): BoclipsApiClient => instance,
-  createInstance: (
-    environment: string,
-    configuredAxios?: AxiosInstance,
-  ): BoclipsApiClient => {
-    if (environment === 'test') {
-      instance = new TestBoclipsApiClient();
-    } else {
-      instance = new HttpBoclipsApiClient(configuredAxios);
-    }
-    return instance;
-  },
-};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-import BoclipsApiClient from './BoclipsApiClient';
+import { HttpBoclipsApiClient } from './HttpBoclipsApiClient';
+import { TestBoclipsApiClient } from './TestBoclipsApiClient';
 
-export default BoclipsApiClient;
+export default {
+  TestBoclipsApiClient,
+  HttpBoclipsApiClient,
+};
