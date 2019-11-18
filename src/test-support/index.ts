7a3e092d8111c43e770ed00bc92877544d538deb
Eva Varga <eva@boclips.com>
2019-11-18 10:09:15 +0000

    Exporting SubjectsFactory

--- src/test-support/index.ts
+++ src/test-support/index.ts
@@ -5,6 +5,7 @@ export * from './FakeBoclipsClient';
 export * from './CollectionsFactory';
 export * from './ContentPartnersFactory';
 export * from './OrderFactory';
+export * from './SubjectsFactory';
 
 export const isATestClient = (
   client: FakeBoclipsClient | ApiBoclipsClient,
