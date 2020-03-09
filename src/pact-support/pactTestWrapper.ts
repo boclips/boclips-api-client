799a90c3574314895f98208fde0a2cc675ab10f9
Jacek Rzeniewicz <jacek@rzrz.pl>
2020-03-09 12:00:31 +0000

    Remove pacts directory before running tests

--- src/pact-support/pactTestWrapper.ts
+++ src/pact-support/pactTestWrapper.ts
@@ -3,10 +3,14 @@ import { ApiBoclipsClient } from '../ApiBoclipsClient';
 import { getAdminLinks } from '../sub-clients/adminLinks/pact/AdminLinksInteractions';
 import { FakeBoclipsClient } from '../test-support/FakeBoclipsClient';
 import { provider } from './pactSetup';
+import rimraf from 'rimraf';
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000; // This is to give the pact mock server time to start
 
-beforeAll(() => provider.setup()); // Create mock provider
+beforeAll(() => {
+  rimraf.sync(provider.opts.dir);
+  return provider.setup(); // Create mock provider
+});
 afterAll(() => provider.finalize()); // Tear down the mock and write the pact
 
 export const withClients = (
