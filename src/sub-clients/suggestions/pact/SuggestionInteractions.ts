f05e6d2271d9126a412f42929f80baa413f8610e
Remigiusz Oleszczuk <remy@boclips.com>
2020-10-19 14:32:42 +0200

    Fix contract test for suggestions
    [#173373008]
    
    Signed-off-by: Marcin Janik <marcin@boclips.com>

--- src/sub-clients/suggestions/pact/SuggestionInteractions.ts
+++ src/sub-clients/suggestions/pact/SuggestionInteractions.ts
@@ -1,34 +1,33 @@
-import { InteractionObject, Matchers } from '@pact-foundation/pact';
-import { eachLike } from '@pact-foundation/pact/dsl/matchers';
+import {InteractionObject, Matchers} from '@pact-foundation/pact';
+import {eachLike} from '@pact-foundation/pact/dsl/matchers';
 import contentTypeRegex from '../../../test-support/HalJsonContentTypeRegex';
 
 export const getSuggestions = (): InteractionObject => ({
-  state: undefined,
-  uponReceiving: 'GET suggestions',
-  withRequest: {
-    method: 'GET',
-    path: '/v1/suggestions',
-    query: 'query=his',
-  },
-  willRespondWith: {
-    status: 200,
-    headers: {
-      'Content-Type': Matchers.term({
-        generate: 'application/hal+json;charset=UTF-8',
-        matcher: contentTypeRegex,
-      }),
+    state: undefined,
+    uponReceiving: 'GET suggestions',
+    withRequest: {
+        method: 'GET',
+        path: '/v1/suggestions',
+        query: 'query=his',
     },
-    body: {
-      suggestionTerm: 'his',
-      channels: eachLike({
-        id: 'channel-id',
-        name: 'The History Channel',
-        _links: null,
-      }),
-      subjects: eachLike({
-        name: 'Art History',
-        _links: null,
-      }),
+    willRespondWith: {
+        status: 200,
+        headers: {
+            'Content-Type': Matchers.term({
+                generate: 'application/hal+json;charset=UTF-8',
+                matcher: contentTypeRegex,
+            }),
+        },
+        body: {
+            suggestionTerm: 'his',
+            channels: eachLike({
+                id: 'channel-id',
+                name: 'The History Channel'
+            }),
+            subjects: eachLike({
+                id: "subject id",
+                name: 'Art History'
+            }),
+        },
     },
-  },
 });
