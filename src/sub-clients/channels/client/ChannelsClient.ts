e7aceafe80c784b3932e0822b7e84721773d31ea
Michal Wit <michal@boclips.com>
2021-02-17 09:53:51 +0100

    Add optional projection to channels client interface
    [#176827296]

--- src/sub-clients/channels/client/ChannelsClient.ts
+++ src/sub-clients/channels/client/ChannelsClient.ts
@@ -1,11 +1,12 @@
 import { ContentCategories } from '../model/ContentCategories';
 import { Channel } from '../model/Channel';
 import { ChannelRequest } from '../model/ChannelRequest';
+import { Projection } from '../../common/model/Projection';
 
 export interface ChannelsClient {
   get(id: string): Promise<Channel>;
 
-  getAll(): Promise<Channel[]>;
+  getAll(projection?: Projection): Promise<Channel[]>;
 
   update(id: string, contentPartner: ChannelRequest): Promise<void>;
 
